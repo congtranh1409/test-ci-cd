3795bc2afa9bb08a484d54de0672555ea5cb264f
DESKTOP-2EP69KG\ADMIN <huycong1409@gmail.com>
2022-05-14 00:01:51 +0700

    add spring project

---
+++ test-cicd-githubActions/src/main/java/com/example/testcicdgithubactions/TestCicdGithubActionsApplication.java
@@ -0,0 +1,13 @@
+package com.example.testcicdgithubactions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestCicdGithubActionsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestCicdGithubActionsApplication.class, args);
+    }
+
+}
---
+++ test-cicd-githubActions/src/test/java/com/example/testcicdgithubactions/TestCicdGithubActionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testcicdgithubactions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestCicdGithubActionsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
